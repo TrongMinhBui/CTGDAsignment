ca4e6df87e8554d1dd042c3cdfacaf2fee89ef61
Trong Bui <145566680+TrongMinhBui@users.noreply.github.com>
2024-09-16 15:48:52 +0200

    first commit
    scripts from cornelius lecture on dots

---
+++ CTGDAssignment/Assets/Scripts/PlayerInputSystem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[UpdateInGroup(typeof(InitializationSystemGroup), OrderLast = true)]
+public partial class PlayerInputSystem : SystemBase
+{
+    private PlayerController InputActions;
+    private Entity Player;
+
+    protected override void OnCreate()
+    {
+        RequireForUpdate<PlayerTag>();
+        RequireForUpdate<PlayerMoveInput>();
+        InputActions = new PlayerController();
+    }
+
+    protected override void OnStartRunning()
+    {
+        InputActions.Enable();
+        Player = SystemAPI.GetSingletonEntity<PlayerTag>();
+        
+    }
+
+    protected override void OnUpdate()
+    {
+        Vector2 moveInput = InputActions.Player.Movement.ReadValue<Vector2>();
+        
+        SystemAPI.SetSingleton(new PlayerMoveInput{ Value = moveInput });
+    }
+
+    protected override void OnStopRunning()
+    {
+        InputActions.Disable();
+        Player = Entity.Null;
+    }
+}
--- CTGDAssignment/Assets/Scripts/Spawner.cs
+++ CTGDAssignment/Assets/Scripts/Spawner.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Entities;
 using UnityEngine;
+using Unity.Mathematics;
 
-public class Spawner : MonoBehaviour
+public struct Spawner : IComponentData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Entity Prefab;
+    public float2 SpawnPosition;
+    public float NextSpawnTime;
+    public float SpawnRate;
 }
